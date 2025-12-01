2ca07f3c5852dae466ff529845badb98edcf6083
Aj-Aralar <ajamyaralar@gmail.com>
2025-12-01 16:02:31 -0500

    build(projectData): Input actual data to pass

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,3 +1,14 @@
 export const projectData = {
+    sectionTitle: 'PROJECTS',
+    sectionBody: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim '],
 
+    projects: [
+        {
+            title: 'Fitspo: AI Powered Fitness Application',
+            shortTitle: 'Fitspo',
+            tools: ['Firebase', 'Express'],
+            myRole: 'Backend and Frontend Developer',
+            description: 'For our Capstone project, Me and my group developed a mobile application that outputs meal plans and workout plans using supervised machine learning'
+        }
+    ]
 }
