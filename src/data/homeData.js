36b836066e52b2922c81c247d5622c5367951685
Aj-Aralar <ajorick.aralar@gmail.com>
2026-01-14 21:32:49 -0500

    build(homeData): Add url property

--- src/data/homeData.js
+++ src/data/homeData.js
@@ -2,11 +2,13 @@ export const homeData = {
     portfolios: [
         {
             title: 'Tech',
+            url: 'tech',
             subtitle: 'Full-Stack Development & Cloud Architecture'
         },
 
         {
             title: 'Music',
+            url: 'music',
             subtitle: 'Production, Performance & Composition'
         }
     ]
